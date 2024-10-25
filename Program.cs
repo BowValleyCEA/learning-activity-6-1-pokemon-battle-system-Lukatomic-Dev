8fc78064f850420df4a8ba8c15a48e3890a80998
Lukatomic <lhoch6@gmail.com>
2024-10-24 20:23:19 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to the world of Pokemon! \n Select your pokemon: \n");
+﻿
+
+using System.Security.Cryptography.X509Certificates;
+
+Pokemon CreateBulbasaur()
+{
+    return new Pokemon
+    {
+        Name = "Bulbasaur",
+        Health = 45,
+        Attack = 50,
+        Defence = 50,
+        Special = 65,
+        Speed = 45,
+    };
+}
+
+Pokemon CreateCharmander()
+{
+    return new Pokemon
+    {
+        Name = "Charmander",
+        Health = 40,
+        Attack = 55,
+        Defence = 45,
+        Special = 50,
+        Speed = 65,
+    };
+}
+
+Pokemon CreateSquirtle()
+{
+    return new Pokemon
+    {
+        Name = "Squirtle",
+        Health = 45,
+        Attack = 50,
+        Defence = 65,
+        Special = 50,
+        Speed = 45,
+    };
+}
+
+Pokemon CreatePikachu()
+{
+    return new Pokemon
+    {
+        Name = "Pikachu",
+        Health = 35,
+        Attack = 55,
+        Defence = 40,
+        Special = 50,
+        Speed = 90,
+    };
+}
+
+Pokemon CreateEevee()
+{
+    return new Pokemon
+    {
+        Name = "Eevee",
+        Health = 55,
+        Attack = 55,
+        Defence = 50,
+        Special = 65,
+        Speed = 55,
+    };
+}
+
+public class Pokemon
+{
+    public string Name;
+    public int Health;
+    public int Attack;
+    public int Defence;
+    public int Special;
+    public int Speed;
+
+    public virtual void BasicAttack()
+    {
+        
+    }
+}
+
+public class Bulbasaur : Pokemon
+{
+    public override void BasicAttack()
+    {
+        base.BasicAttack();
+    }
+}
